920a8102028de9ce231e1a1fea75c80bb2a4f181
HUNED053 <khambhatijuzer11@gmail.com>
2024-07-21 12:37:49 +0530

    Create CALC.cpp

---
+++ CALC.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+  int a,b,c;
+  cout<<"enter number a & b";
+  cin>>a>>b;
+  cout<<"addition of a&b is"<<a+b;
+  cout<<"subraction of a&b is"<<a-b;
+  cout<<"multiplication of a&b is"<<a*b;
+  cout<<"division of a&b is"<<a/b;
+}
